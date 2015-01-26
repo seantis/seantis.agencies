0d2435509582f2e05d2a8e367ef50891b40ec434
Marc Sommerhalder <marc.sommerhalder@seantis.ch>
2015-01-26 15:31:33 +0100

    Do not test svg in pdf report

--- seantis/agencies/tests/test_pdf_export.py
+++ seantis/agencies/tests/test_pdf_export.py
@@ -1,9 +1,13 @@
+import mock
+
 from seantis.agencies import tests
 
 
 class TestPdfExport(tests.BrowserTestCase):
 
-    def test_export_pdf_empty(self):
+    @mock.patch('kantonzugpdf.ReportZug.get_logo')
+    def test_export_pdf_empty(self, get_logo):
+        get_logo.return_value = None
         browser = self.new_browser()
         browser.open('/pdfexport-agencies')
 
